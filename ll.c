96755555e6dacb073ec55dd974e92b6120e8ae7d
kimv06 <monalpodder12@gmail.com>
2022-05-06 12:46:24 +0530

    modified functions

---
+++ ll.c
@@ -0,0 +1,34 @@
+#include "ll.h"
+Node* create_node(int value)
+{
+    Node* temp;
+    temp = (Node*)malloc(sizeof(Node));
+    temp->data = value;
+    temp->next = NULL;
+
+    return temp;
+}
+void append(Node** head, int value)
+{
+    Node* temp = create_node(value);
+    Node* current = *head;
+    if (*head == NULL)
+    {
+        *head = temp;
+        return;
+    }
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = temp;
+}
+void traverse(Node* pthead)
+{
+    while (pthead != NULL)
+    {
+        printf("%d ", pthead->data);
+        pthead = pthead->next;
+    }
+    printf("\n");
+}
