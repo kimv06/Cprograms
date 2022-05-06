768d876a587fea799833154c007dd06d865d96f0
kimv06 <monalpodder12@gmail.com>
2022-05-06 19:06:35 +0530

    swapping three numbers using pointers

--- cyclicswapusingpointer.c
+++ cyclicswapusingpointer.c
@@ -15,8 +15,8 @@ int main()
     int c;
     printf("Enter the value of a,b and c respectively\n");
     scanf("%d%d%d",&a,&b,&c);
-    printf("The values of a,b,c before swapping:  a = %d b = %d c = %d ",a,b,c);
+    printf("The values of a,b,c before swapping: a = %d b = %d c = %d ",a,b,c);
     CyclicSwap(&a,&b,&c);
-    printf("\nThe values of a,b,c after swapping:  a = %d b = %d c = %d ",a,b,c);
+    printf("\nThe values of a,b,c after swapping: a = %d b = %d c = %d ",a,b,c);
     return 0;
 }
